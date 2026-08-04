c28bd2c35bfa25e8cf0b66852a52f143beb1f660
Rahul Kumar Mandal <iamrahul6203@gmail.com>
2026-08-04 12:27:22 +0530

    feat: add Positive.java to check if a number is positive, negative, or zero

---
+++ track/Practice/Module1/Positive.java
@@ -0,0 +1,12 @@
+public class Positive {
+    public static void main(String[] args) {
+        int num = 101;
+        if (num == 0) {
+            System.out.println("number is zero");
+        } else if (num > 0) {
+            System.out.println("number is positive");
+        } else {
+            System.out.println("number is negative");
+        }
+    }
+}
