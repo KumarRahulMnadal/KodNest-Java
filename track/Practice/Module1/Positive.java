4dea9eade124499ef2c82dfa2f3d6aa3367a81d8
Rahul Kumar Mandal <iamrahul6203@gmail.com>
2026-08-04 13:01:29 +0530

    feat: add Positive.java to implement number classification and score comparison logic

--- track/Practice/Module1/Positive.java
+++ track/Practice/Module1/Positive.java
@@ -1,12 +1,27 @@
 public class Positive {
     public static void main(String[] args) {
-        int num = 101;
-        if (num == 0) {
-            System.out.println("number is zero");
-        } else if (num > 0) {
-            System.out.println("number is positive");
+        int num = -7;
+        int firstScore = 18;
+        int lastScore = 25;
+
+        if (num > 0) {
+            System.out.println("number type: positive");
+        } else if (num < 0) {
+            System.out.println("number type: negative");
         } else {
-            System.out.println("number is negative");
+            System.out.println("number type: zero");
+        }
+
+        if(num % 2 == 0) {
+            System.out.println("Parity: Even");
+        } else {
+            System.out.println("Parity: Odd");
+        }
+
+        if (firstScore >= lastScore) {
+            System.out.println("Larger score:" + firstScore);
+        } else {
+            System.out.println("Larger score:" + lastScore);
         }
     }
 }
