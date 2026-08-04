b624fb9699195094736e0d2fa107644803a35b38
Rahul Kumar Mandal <iamrahul6203@gmail.com>
2026-08-04 11:31:58 +0530

    feat: add Practice2 class with scheduled day printing logic

---
+++ track/Practice/Module1/Practice2.java
@@ -0,0 +1,9 @@
+public class Practice2 {
+  public static void main(String[] args) {
+    System.out.println("Monday");
+    System.out.println("Tuesday"+"Wednesday");
+    System.out.println("Thursday");
+    System.out.println("Friday"+"Saturday");
+    System.out.println("sunday");
+  }
+}
