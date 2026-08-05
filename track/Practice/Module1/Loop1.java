939a7de9e9724b52a8017403a5d4cf21050a141d
Rahul Kumar Mandal <iamrahul6203@gmail.com>
2026-08-05 12:19:48 +0530

    feat: add Loop1 class with basic for loop printing 1 to 5

---
+++ track/Practice/Module1/Loop1.java
@@ -0,0 +1,9 @@
+package Practice.Module1;
+
+public class Loop1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <=5; i++) {
+            System.out.println(i);
+        }
+    }
+}
