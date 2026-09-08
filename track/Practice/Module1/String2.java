8e1726d7275385cac87188c3a39d115f29bb1758
Rahul Kumar Mandal <iamrahul6203@gmail.com>
2026-09-09 00:53:42 +0530

    feat: add String2 practice file demonstrating string manipulation methods

---
+++ track/Practice/Module1/String2.java
@@ -0,0 +1,24 @@
+package track.Practice.Module1;
+
+public class String2 {
+    public static void main(String[] args) {
+        String str = "KodNest Technologies";
+
+        System.out.println(str);
+        System.out.println(str.toLowerCase());
+        System.out.println(str.toUpperCase());
+         System.out.println(str.charAt(3));
+        System.out.println(str.charAt(99));
+        System.out.println(str.contains("Nest"));
+        System.out.println(str.contains("nest"));
+        System.out.println(str.endsWith("Kod"));
+        System.out.println(str.endsWith("ies"));
+        System.out.println(str.length());
+        System.out.println(str.indexOf('K'));
+        System.out.println(str.indexOf('e'));
+        System.out.println(str.length());
+        System.out.println(str.replace('e','A'));
+
+    }
+    
+}
